f240b5f7a005d72913d8e03975be1dbf89a49267
Igor <star.shadow0707@gmail.com>
2023-07-21 18:13:12 +0300

    feat: add blocker for fptls.com

--- main.js
+++ main.js
@@ -1,5 +1,50 @@
 import './style.css'
 document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
+        <div class="checkbox-block">
+       
+        <input type="checkbox" id="myCheckbox">
+        Block FPJS
+      </div>
 `
+
+document.addEventListener("DOMContentLoaded", () => {
+    const checkbox = document.querySelector("#myCheckbox");
+    if (checkbox) {
+        const savedCheckboxValue = localStorage.getItem("myCheckboxValue");
+        checkbox.checked = savedCheckboxValue === 'true';
+        if (savedCheckboxValue === 'true'){
+            enableRule()
+        }
+
+        checkbox.addEventListener("change", handleCheckboxChange);
+    }
+
+    function handleCheckboxChange() {
+        localStorage.setItem("myCheckboxValue", checkbox.checked);
+
+        if (checkbox.checked) {
+            enableRule()
+
+        } else {
+            disableRule()
+        }
+    }
+
+    function disableRule() {
+        chrome.declarativeNetRequest.updateEnabledRulesets(
+            {
+                disableRulesetIds: ["ruleset_1"],
+            },
+            () => {}
+        );
+    }
+
+    function enableRule() {
+        chrome.declarativeNetRequest.updateEnabledRulesets(
+            {
+                enableRulesetIds: ["ruleset_1"],
+            },
+            () => {}
+        );
+    }
+});
